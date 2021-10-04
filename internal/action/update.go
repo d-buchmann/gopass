10b83e97bc7e96a81f00ef0c878f6dc91af19d16
d-buchmann <buchmann@ticmed.de>
2021-10-04 13:11:27 +0200

    remove stale os check

--- internal/action/update.go
+++ internal/action/update.go
@@ -22,10 +22,6 @@ func (s *Action) Update(c *cli.Context) error {
 		return nil
 	}
 
-	if runtime.GOOS == "windows" {
-		return fmt.Errorf("gopass update is not supported on windows (#1722)")
-	}
-
 	out.Printf(ctx, "⚒ Checking for available updates ...")
 	if err := updater.Update(ctx, s.version); err != nil {
 		return ExitError(ExitUnknown, err, "Failed to update gopass: %s", err)
